ab3d51a1dc09328efa9fbc4c9e236f775a52ce62
soulfulolya20 <soulful.olya@gmail.com>
2023-10-31 22:38:01 +0300

    fixed finish

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -8,7 +8,6 @@ using UnityEngine.UI;
 public class FinishZone : NetworkBehaviour
 {
     private List<NetworkObject> playersInFinish = new List<NetworkObject>();
-    private List<NetworkObject> allPlayers = new List<NetworkObject>();
     private AudioSource finishSound;
 
     public Text[] playerPlaceTexts;
@@ -27,7 +26,12 @@ public class FinishZone : NetworkBehaviour
             if (playerNetworkObject != null)
             {
                 // Записываем игрока, достигшего финиша, в список
-                playersInFinish.Add(playerNetworkObject);
+                if (!playersInFinish.Contains(playerNetworkObject))
+                {
+                    playersInFinish.Add(playerNetworkObject);
+                    // Показываем информацию о текущем игроке и его месте
+                    ShowCurrentPlayerPlace(playerNetworkObject);
+                }
 
                 // Отключаем возможность движения
                 PlayerBase playerController = other.GetComponent<PlayerBase>();
@@ -38,15 +42,13 @@ public class FinishZone : NetworkBehaviour
                 }
 
                 // Воспроизводим звук финиша
-                finishSound.Play();
-
-                // Показываем информацию о текущем игроке и его месте
-                ShowCurrentPlayerPlace(playerNetworkObject);
+               // finishSound.Play();
             }
 
             // Если количество игроков в списке достигло общего числа игроков, переходим на стартовую сцену
             if (playersInFinish.Count == GameManager.CountPlayers())
             {
+                NetworkManager.Singleton.SceneManager.LoadScene("StartScene", LoadSceneMode.Single);
                 ServerGameOverServerRpc();
             }
         }
@@ -61,7 +63,9 @@ public class FinishZone : NetworkBehaviour
         {
             if (string.IsNullOrEmpty(playerPlaceText.text))
             {
-                playerPlaceText.text = "Player " + playerName + " took place " + playerPlace;
+                // playerPlaceText.text = "Player " + playerName + " took place " + playerPlace;
+                playerPlaceText.text = "playersinfinish=" + playersInFinish.Count + " allPlayers=" +
+                                       GameManager.CountPlayers();
                 break;
             }
         }
@@ -78,7 +82,9 @@ public class FinishZone : NetworkBehaviour
     {
         // Отключите всех клиентов и сервер перед переходом на стартовую сцену
         Cleanup();
-
+        NetworkManager.Singleton.SceneManager.LoadScene("StartScene", LoadSceneMode.Single);
+        
+        SceneManager.LoadScene("StartScene");
         // Задержка перед переходом на стартовую сцену
         StartCoroutine(LoadStartSceneDelayed());
     }
@@ -86,6 +92,7 @@ public class FinishZone : NetworkBehaviour
 
     private IEnumerator LoadStartSceneDelayed()
     {
+        SceneManager.LoadScene("StartScene");
         yield return new WaitForSeconds(5f);
         SceneManager.LoadScene("StartScene");
     }
